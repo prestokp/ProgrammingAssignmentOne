ceab423d2b4f82112d5e7372f21b7048bc49890b
kirkpreston <prestokp@rose-hulman.edu>
2019-06-15 21:58:22 -0500

    Programming Assignment One Commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+
+    double gradeAverage;   //this code block defines all of the variables that will be used throughout the program
+    string firstName;
+    string lastName;
+    string firstAssignment;
+    string secondAssignment;
+    string thirdAssignment;
+    int firstGrade;
+    int secondGrade;
+    int thirdGrade;
+
+    //program specifications call for the variables to be put in array so the following code will be prototype code
+    //string studentName[2] = {firstName, lastName};
+    //string nameAssignments[3] = {firstAssignment, secondAssignment, thirdAssignment}
+    //int grades[3] = {firstGrade, secondGrade, thirdGrade};
+
+    
+}
