980ed01ed1e60df6df256c571a3ba716edbaeab4
kirkpreston <prestokp@rose-hulman.edu>
2019-06-18 12:45:51 -0500

    Programming Assignment One mockup

--- main.cpp
+++ main.cpp
@@ -5,17 +5,15 @@
 #include <string>
 using namespace std;
 
+const int NUM_ASSIGNMENT = 3;
+const int GRADE_NUMBER = 3;
 int main() {
 
     double gradeAverage;   //this code block defines all of the variables that will be used throughout the program
-    string firstName;
-    string lastName;
-    string firstAssignment;
-    string secondAssignment;
-    string thirdAssignment;
-    int firstGrade;
-    int secondGrade;
-    int thirdGrade;
+    string studentName;
+    string assignmentName[NUM_ASSIGNMENT];
+    int grades[GRADE_NUMBER];
+
 
     //program specifications call for the variables to be put in array so the following code will be prototype code
     //string studentName[2] = {firstName, lastName};
@@ -23,43 +21,43 @@ int main() {
     //int grades[3] = {firstGrade, secondGrade, thirdGrade};
 
     cout<<"What is your first and last name: ";
-    cin>> firstName>>lastName;
+    getline(cin, studentName);
     //one line code to get the entire line: getline(cin, studentName);
 
 
 
     cout<<"Enter first assignment name: ";
-    cin>>firstAssignment;
+    getline(cin, assignmentName[0]);
 
     cout<<"Enter second assignment name: ";
-    cin>>secondAssignment;
+    getline(cin, assignmentName[1]);
 
     cout<<"Enter third assignment name: ";
-    cin>>thirdAssignment;
+    getline(cin, assignmentName[2]);
 
 
 
     cout<<"Enter first grade: ";   //These are the iostreams that prompt the user to enter their grades in
-    cin>>firstGrade;               //The grades must be entered in as integers, needs a more descriptive prompt
+    cin>>grades[0];               //The grades must be entered in as integers, needs a more descriptive prompt
 
     cout<<"Enter second grade: ";
-    cin>>secondGrade;
+    cin>>grades[1];
 
     cout<<"Enter third grade: ";
-    cin>>thirdGrade;
+    cin>>grades[2];
 
     //one line code that will get the grades from a single line: getline(cin, grades);
 
-    gradeAverage = (firstGrade + static_cast<double>(secondGrade)+thirdGrade)/(3.0); //Average expression, static type cast
+    gradeAverage = (grades[0] + static_cast<double>(grades[1])+grades[2])/(3.0); //Average expression, static type cast
     //as extra redundancy to ensure the data type gets converted to a double
 
-    cout<<"The average for "<<firstName<<" "<<lastName<<" is: "<<fixed<<showpoint<<setprecision(1)<<gradeAverage<<endl; //when array solution is implemented the output variable will
+    cout<<"The average for "<<studentName<<" "<<"is: "<<fixed<<showpoint<<setprecision(1)<<gradeAverage<<endl; //when array solution is implemented the output variable will
     //simply be studentName
 
     cout<<"Here are your grades: "<<endl;
-    cout<<firstAssignment<<": "<<firstGrade<<endl;
-    cout<<secondAssignment<<": "<<secondGrade<<endl;
-    cout<<thirdAssignment<<": "<<thirdGrade<<endl;
+    cout<<assignmentName[0]<<": "<<firstGrade<<endl;
+    cout<<assignmentName[1]<<": "<<secondGrade<<endl;
+    cout<<assignmentName[2]<<": "<<thirdGrade<<endl;
     cout<<endl<<endl;
     cout<<"Thank you for playing. ";
 
